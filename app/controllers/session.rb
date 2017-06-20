5edeea6118f1bd49d5140919594b41c613cc0406
sushant12 <sussyoung9@gmail.com>
2017-06-20 19:04:00 +0545

    logout added

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,8 +1,14 @@
 Fuitter::App.controllers :session do
 
-  get :auth, '/auth/:provider/callback' do
+  get :auth, map: '/auth/:provider/callback' do
     auth = request.env['omniauth.auth']
     account = Account.create_or_find_from_omniauth(auth)
+    session[:account_id] = account.id
+    redirect '/'
+  end
+
+  get :destroy, map: 'destroy' do
+    session[:account_id] = nil
     redirect '/'
   end
 
