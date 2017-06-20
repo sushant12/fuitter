58879ac0195fed93a7793c19a180581f46d84ec0
sushant12 <sussyoung9@gmail.com>
2017-06-20 18:37:47 +0545

    decople finding and creating in session controller

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -2,7 +2,7 @@ Fuitter::App.controllers :session do
 
   get :auth, '/auth/:provider/callback' do
     auth = request.env['omniauth.auth']
-    account = Account.find(uid: auth['uid']) || Account.create_with_omniauth(auth)
+    account = Account.create_or_find_from_omniauth(auth)
     redirect '/'
   end
 
--- models/account.rb
+++ models/account.rb
@@ -1,6 +1,6 @@
 class Account < Sequel::Model
-  def self.create_with_omniauth(auth)
-    create do |account|
+  def self.create_or_find_from_omniauth(auth)
+    find(uid: auth['uid']) || create do |account|
       account.uid      = auth["uid"]
       account.name    = auth["info"]["name"] if auth["info"]
       account.email    = auth["info"]["email"] if auth["info"]
