abe82983d86020b6312718cde07538b210a40532
sushant12 <sussyoung9@gmail.com>
2017-06-21 19:36:10 +0545

    save facebook page into db

--- models/facebook_page.rb
+++ models/facebook_page.rb
@@ -1,13 +1,3 @@
 class FacebookPage < Sequel::Model
   many_to_one :account
-
-  def self.save(pages)
-    # ap @facebook_pages
-    pages.each do |page|
-      create do |fb_page|
-        fb_page.name = page['name']
-        fb_page.account_id = session[:account_id]
-      end
-    end
-  end
 end
