d386dc0315d2c82cb53b9e99bb8de71e739a15b7
sushant12 <sussyoung9@gmail.com>
2017-06-21 08:33:23 +0545

    fetch data for every page

--- app/controllers/template.rb
+++ app/controllers/template.rb
@@ -6,15 +6,18 @@ Fuitter::App.controllers :template do
   end
 
   get :template_about, map: '/:token/about' do
-
+    @about = get_data_for_about(Koala::Facebook::API.new(params[:token], ENV['FACEBOOK_SECRET']))
+    render 'about'
   end
 
   get :template_contact, map: '/:token/contact' do
-
+    @contact = get_data_for_contact(Koala::Facebook::API.new(params[:token], ENV['FACEBOOK_SECRET']))
+    render 'contact'
   end
 
   get :template_news, map: '/:token/news' do
-
+    @news = get_data_for_news(Koala::Facebook::API.new(params[:token], ENV['FACEBOOK_SECRET']))
+    render 'news'
   end
 
   get :template_events, map: '/:token/events' do
@@ -22,7 +25,8 @@ Fuitter::App.controllers :template do
   end
 
   get :template_gallery, map: '/:token/gallery' do
-
+    @albums = get_data_for_gallery(Koala::Facebook::API.new(params[:token], ENV['FACEBOOK_SECRET']))
+    render 'gallery'
   end
 
 end
@@ -31,3 +35,26 @@ def get_data_for_home(obj)
     fields = {fields: 'name,about,description_html,cover,link,location,website'}
     obj.get_object('me',fields)
 end
+
+def get_data_for_about(obj)
+  fields = {fields: 'name,about,description_html,cover'}
+  obj.get_object('me',fields)
+end
+
+def get_data_for_contact(obj)
+  fields = {fields: 'name,about,link,location,website'}
+  obj.get_object('me', fields)
+end
+
+def get_data_for_news(obj)
+  obj.get_connection('me','feed')
+end
+
+def get_data_for_events(obj)
+  obj.get_connection('me','event')
+end
+
+def get_data_for_gallery(obj)
+  obj.get_connection('me','albums')
+end
+
