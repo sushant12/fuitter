e1afe8274a21576ebd6d026952902504a00dce40
sushant12 <sussyoung9@gmail.com>
2018-08-11 10:47:28 +0545

    wrote specs for json web tokens

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,11 +2,11 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      body = JWT.decode(token, Rails.application.secrets.secret_key_base, true, {algorithm: 'HS256'})[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
---
+++ spec/lib/json_web_token_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+# require_relative '../../lib/'
+describe JsonWebToken do
+  
+  let(:token) { JsonWebToken.encode({user_id: 1}) }
+
+  it 'should verify token' do
+    payload = JsonWebToken.decode(token)
+    expect(payload[:user_id]).to eq(1)
+  end
+
+  it "should return nil" do
+    payload = JsonWebToken.decode('sadf')
+    expect(payload).to eq(nil)
+  end
+end
